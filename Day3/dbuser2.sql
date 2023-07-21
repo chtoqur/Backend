ebc60cfcb2688979ba83254a5c299be47f5ca3a3
Choi Iseo <chxiseo@naver.com>
2023-07-21 17:29:43 +0900

    first commit

---
+++ Day3/dbuser2.sql
@@ -0,0 +1,8 @@
+ 
+ select * from bbs_tbl;
+ select * from user_tbl;
+ 
+ ALTER TABLE USER_TBL DROP COLUMN ADDRESS;
+ ALTER TABLE USER_TBL ADD ADDRESS VARCHAR2(300) DEFAULT '서울특별시' NOT NULL;
+ 
+ commit;
